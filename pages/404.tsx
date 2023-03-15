6c0bcd2b900d1855d312aed7dc7646b0933426de
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-03-15 15:10:23 +0100

    FEATURE - change 404 text

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,6 @@
 
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 
@@ -12,23 +11,26 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Erreur 404</title>
+        <title>Le Système Solaire - Erreur 404</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
           <h1>Erreur 404</h1>
-          <p>Le système solaire est vaste et on s'y perd facilement. Mais pas de panique voici le chemin :</p>
+          <p>Le système solaire est vaste et on s'y perd facilement.</p>
+          <p>
+            Surtout ne paniquez pas, et laissez-vous guider via ce trou de ver :
+          </p>
         </div>
 
         <div className={styles.flex}>
           <Link href="/" className={styles.card}>
             <h2 className={inter.className}>
-              Vers la Terre <span>-&gt;</span>
+              Par ici <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Emprunter ce raccourci dans l'espace temps pour retrouvez votre chemin.
+              Un raccourci dans l'espace-temps pour retrouvez votre chemin.
             </p>
           </Link>
         </div>
