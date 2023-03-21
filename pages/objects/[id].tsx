daffefa4205d5921015a2a1f1683ab74e93a5d72
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-03-21 14:16:54 +0100

    FEATURE - copy bodies pages to connect props with graphQL API

---
+++ pages/objects/[id].tsx
@@ -0,0 +1,66 @@
+// Description: details of a body in the solar system - /bodies/:id
+
+import type { GetStaticPaths, GetStaticProps } from 'next/types';
+import { HeadDocument, MainDocument } from '@/layout';
+import { loadBodies, loadOneBody } from '@/lib/loadData';
+import formatName from '@/lib/formatName';
+import Details from '@/components/Details';
+
+interface PropsType {
+  body: BodyType | string;
+}
+
+export default function Body({ body }: PropsType) {
+  const bodyName = typeof body !== 'string' && formatName(body.name);
+
+  return (
+    <>
+      <HeadDocument
+        titlePage={`Le Système Solaire - ${bodyName}`}
+        descriptionPage={`${bodyName} : tous les détails de cet objet céleste situé dans notre système solaire`}
+      />
+
+      <MainDocument title={bodyName || 'Erreur interne'}>
+        {typeof body === 'string' ? (
+          <p>{body}</p>
+        ) : (
+          <div className="description">
+            <Details body={body} />
+          </div>
+        )}
+      </MainDocument>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const bodies = await loadBodies();
+
+  if (!bodies || typeof bodies === 'string')
+    return { paths: [], fallback: false };
+
+  const paths = bodies.map((body) => ({
+    params: { id: body.id },
+  }));
+
+  return {
+    paths,
+    fallback: false, // default value in case of unfound id in paths. false = 404
+    // revalidate: 7 * 24 * 60 * 60, // refresh data every 7 days
+  };
+};
+
+export const getStaticProps: GetStaticProps<PropsType> = async ({ params }) => {
+  const body = await loadOneBody(params?.id as string);
+
+  if (!body) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { body },
+    revalidate: 7 * 24 * 60 * 60, // refresh data every 7 days
+  };
+};
---
+++ pages/objects/index.tsx
@@ -0,0 +1,59 @@
+// Description: all bodies in the solar system - /bodies
+
+import type { GetStaticProps } from 'next/types';
+
+import { useRouter } from 'next/router';
+
+import { HeadDocument, MainDocument } from '@/layout';
+import { Card } from '@/components';
+
+import { loadBodies } from '@/lib/loadData';
+import formatName from '@/lib/formatName';
+
+interface PropsType {
+  bodies: BodyType[] | string;
+}
+
+export default function Bodies({ bodies }: PropsType) {
+  const { asPath } = useRouter();
+
+  return (
+    <>
+      <HeadDocument
+        titlePage="Les objets du Système Solaire"
+        descriptionPage="Tous les objets célestes qui composent notre système solaire"
+      />
+
+      <MainDocument title="Les objets célestes de notre système solaire">
+        <div className="flex">
+          {typeof bodies === 'string' ? (
+            <p>{bodies}</p>
+          ) : (
+            bodies.map((body) => (
+              <Card
+                key={body.id}
+                cardURL={`${asPath}/${body.id}`}
+                cardTitle={formatName(body.name)}
+                cardLegend={body.englishName}
+                cardImage="/favicon.ico"
+                CardImageAlt={body.name}
+              />
+            ))
+          )}
+        </div>
+      </MainDocument>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PropsType> = async () => {
+  const bodies = await loadBodies({
+    filter: [['bodyType', 'cs', 'Planet']],
+    order: ['sideralOrbit', 'asc'],
+  });
+
+  return {
+    props: { bodies },
+    revalidate: 7 * 24 * 60 * 60, // refresh data every 7 days
+  };
+};
